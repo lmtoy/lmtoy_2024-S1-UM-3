77029e00a44f4a5a3ee247c866e1895de3c0a67e
Peter Teuben <teuben@gmail.com>
2024-03-17 20:00:25 -0400

    add more

--- mk_runs.py
+++ mk_runs.py
@@ -15,16 +15,33 @@ on["Core_41"] =  [ 112173, 112185,]
 
 on["Core_42"] =  [ 112187, 112191,]
 
+on["Core_43"] =  [ 113275, 113277,]
+
+on["Core_56"] =  [ 113281, 113283,]
+
+
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 pars1["Core_41"] = ""
 pars1["Core_42"] = ""
+pars1["Core_43"] = ""
+pars1["Core_56"] = ""
+
+
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
 pars2["Core_41"] = ""
 pars2["Core_42"] = ""
+pars2["Core_43"] = ""
+pars2["Core_56"] = ""
+
+pars3 = {}
+pars3["Core_41"] = ""
+pars3["Core_42"] = ""
+pars3["Core_43"] = ""
+pars3["Core_56"] = ""
 
 if __name__ == '__main__':    
-    runs.mk_runs(project, on, pars1, pars2, sys.argv)
+    runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
